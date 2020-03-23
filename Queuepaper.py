9bc5d4810b730a4f8ec977b3383c2c442d15fdea
Michael Suter <suterm@hartwick.edu>
2020-03-23 18:06:11 -0400

    almost finished, Stack

--- Queuepaper.py
+++ Queuepaper.py
@@ -1,3 +1,25 @@
 # Michael Suter
 # using Queue, apply it to scenario
 # 3-10-20
+
+from StackQ import Queue
+
+
+def menu():
+
+
+
+
+
+
+
+
+
+
+print("""Enter 1 to add, 
+    Enter 2 to sell,
+    Enter 3 to see profit, 
+    Enter 4 see the amount of cases in inventory, 
+    Enter 5 to Quit""")
+
+menu()
--- Stackpaper.py
+++ Stackpaper.py
@@ -6,38 +6,34 @@ from StackQ import Stack
 
 
 def menu():
+    inventory = Stack()
     choice = int(input("Enter a number between 1-5... >>"))
-    while choice != 5:
-        inventory = Stack
-        while choice < 1 or choice > 5:
-            if choice == 1:
-                print("Disclaimer: The price of producing and shipping a case of paper is $20")
-                add_to_inventory = int(input("How many cases of paper would you like to add to inventory? >>"))
-                inventory = Stack.the_stack
-                inventory = inventory + add_to_inventory
-                price = 20
-                cost = price * inventory
-                print(f"The cost of this order is {cost}")
-                Stack.push(inventory, add_to_inventory)
-                print(f"there are now {inventory} cases of paper in the inventory.")
-            elif choice == 2:
-                print("Disclaimer: We sell the cases for $2 more by default")
-                sell_amount = int(input(f"How many cases of paper do you want to sell out of the {inventory} in the inventory? >>"))
-                sell_price = 22
-                total_sale = sell_amount * sell_price
-                print(f"You just sold {sell_amount} cases for {total_sale}")
-                revenue = total_sale - cost
-                print(f"")
-                inventory = inventory - sell_amount
-                Stack.pop(inventory)
-                return
-            elif choice == 3:
-                print(f"the total revenue is {revenue}")
-                return
-            else:
-                if choice == 4:
-                    print(f"There are {inventory} cases available in the inventory")
-                    return
+    while choice >= 1 or choice <= 5:
+        if choice == 1:
+            qty = int(input("How many cases of paper would you like to add to inventory? >>"))
+            price = int(input("What is the individual cost of these cases? >> $"))
+            inventory.push(qty)
+            cost = price * qty
+            print(f"The cost of this order is ${cost}")
+            print(f"there are now {qty} more cases of paper in the inventory worth {cost}")
+            menu()
+        elif choice == 2:
+            print("Take a look at the inventory below...")
+            print(inventory.the_stack)
+            sell_amount = int(input(f"How many cases of paper do you want to sell out of the inventory? >>"))
+            sell_price = int(input("How much are you selling each case for?>>"))
+            total_sale = sell_amount * sell_price
+            revenue = total_sale - cost
+            print(f"You just sold {sell_amount} cases for {total_sale}")
+            inventory.pop()
+            menu()
+        elif choice == 3:
+            print(f"the total revenue is {revenue}.")
+            menu()
+        else:
+            if choice == 4:
+                print("Here is a list of cases left in the inventory", inventory.the_stack)
+                menu()
 
 
 print("""Enter 1 to add, 
