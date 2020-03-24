ead86d552b96999b3fe50d183a4ac4e58171cfa9
Michael Suter <suterm@hartwick.edu>
2020-03-24 11:09:20 -0400

    1

--- Queuepaper.py
+++ Queuepaper.py
@@ -6,7 +6,34 @@ from StackQ import Queue
 
 
 def menu():
-
+    inventory = Queue()
+    choice = int(input("Enter a number between 1-5... >>"))
+    while choice >= 1 or choice <= 5:
+        if choice == 1:
+            qty = int(input("How many cases of paper would you like to add to inventory? >>"))
+            price = int(input("What is the individual cost of these cases? >> $"))
+            inventory.push(qty)
+            cost = price * qty
+            print(f"The cost of this order is ${cost}")
+            print(f"there are now {qty} more cases of paper in the inventory worth {cost}")
+            menu()
+        elif choice == 2:
+            print("Take a look at the inventory below...")
+            print(inventory.myList)
+            sell_amount = int(input(f"How many cases of paper do you want to sell out of the inventory? >>"))
+            sell_price = int(input("How much are you selling each case for?>>"))
+            total_sale = sell_amount * sell_price
+            revenue = total_sale - cost
+            print(f"You just sold {sell_amount} cases for {total_sale}")
+            inventory.pop()
+            menu()
+        elif choice == 3:
+            print(f"the total revenue is {revenue}.")
+            menu()
+        else:
+            if choice == 4:
+                print("Here is a list of cases left in the inventory", inventory.myList)
+                menu()
 
 
 
